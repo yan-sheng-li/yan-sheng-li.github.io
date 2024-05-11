f3dc0354bd6f6ff428916ba1a915d94f939f1ec5
liyansheng <1761724207@qq.com>
2024-05-11 16:59:40 +0800

    deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    title: 'Hello VuePress',
-    description: 'Just playing around',
+    title: '技术探索空间',
+    description: '实用技术要点汇总',
   }
---
+++ readme.md
@@ -0,0 +1,3 @@
+## 安装教程👉[官网](https://vuepress.vuejs.org/zh/)
+
+## 部署教程👉[参考](https://github.com/jenkey2011/vuepress-deploy/blob/master/README.zh-CN.md)
