7a27ac67645559ee2e3b466a910e995ab158c25a
liyansheng <1761724207@qq.com>
2024-05-11 16:09:26 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:yan-sheng-li/yan-sheng-li.git main
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+git push -f git@github.com:yan-sheng-li/yan-sheng-li.github.io.git main
+
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
