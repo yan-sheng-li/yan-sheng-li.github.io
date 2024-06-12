a95cb28b7a4e513f6593e2052227753bc5fcfe07
liyansheng <1761724207@qq.com>
2024-06-12 22:57:57 +0800

    更新指引

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -13,7 +13,8 @@ module.exports = {
     description: '实用技术要点汇总',
     themeConfig: {
       nav: [
-        { text: '🏠首页', link: '/' },
+        // { text: '🏠首页', link: '/' },
+        { text: '💪博主帮一手', link: 'https://qm.qq.com/cgi-bin/qm/qr?k=NZUoWMzd3PQLWwxRGMiBNYEnVkEdNq__&jump_from=webapi&authKey=kgAofDqUzgwMCSX+UQQwxf837zMeWFGGmo4iIcbgkklW2pdfmVOlxPWAK6sMYMaC' },
         { text: '📦项目大全', link: '/project/' },
         { text: '🐞远程调试', link: 'http://liyansheng.top/remote_help/' }
       ],
