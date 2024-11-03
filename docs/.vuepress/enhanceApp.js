eeab3f62a6365944c3c4f453ac891cc870e4f21a
liyansheng <1761724207@qq.com>
2024-11-04 00:19:32 +0800

    项目优化

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -9,7 +9,6 @@ import Cook from './components/Cook.vue'
 import PasswordProtected from './components/PasswordProtected.vue'
 import SlideVerify from 'vue-monoplasty-slide-verify';
 import SlideProtected from './components/SlideProtected.vue'
-import Carousel3d   from 'vue-carousel-3d';
 
 
 
@@ -24,6 +23,5 @@ export default ({ Vue  }) => {
   Vue.component('ScrollingNotice', ScrollingNotice)
   Vue.component('CustomFooter', CustomFooter)
   Vue.use(SlideVerify);
-  Vue.use(Carousel3d);
   Vue.component('SlideProtected', SlideProtected)
 }
