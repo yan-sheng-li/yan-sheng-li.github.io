9e382d7b1ef55e64ecba3d14c064498fbf2cfcf1
liyansheng <1761724207@qq.com>
2025-06-30 13:22:40 +0800

    fix: 组件命名调整

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -4,7 +4,7 @@ import MyGlobalComponent from './components/MyGlobalComponent.vue'
 import gzh from './components/gzh.vue'
 import ScrollingNotice from './components/ScrollingNotice.vue'
 import CustomFooter from './components/CustomFooter.vue'
-import S from './components/s.vue'
+import Find from './components/Find.vue'
 import Cook from './components/Cook.vue'
 import PasswordProtected from './components/PasswordProtected.vue'
 import SlideVerify from 'vue-monoplasty-slide-verify';
@@ -54,7 +54,7 @@ export default ({ Vue,isServer}) => {
   // 全局注册组件
   Vue.component('MyGlobalComponent', MyGlobalComponent);
   Vue.component('gzh', gzh)
-  Vue.component('S', S)
+  Vue.component('Find', Find)
   Vue.component('PasswordProtected', PasswordProtected)
   Vue.component('Cook', Cook)
   Vue.component('ScrollingNotice', ScrollingNotice)
--- docs/mm/readme.md
+++ docs/mm/readme.md
@@ -1,7 +1,7 @@
 <div style="text-align:center">
     <img style="width:30%" src="http://cdn.qiniu.liyansheng.top/img/7404018d8f361a88.png" />
 </div>
-<S/>
+<Find />
 
 <PaymentButton :productId="101" />
 
